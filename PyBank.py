1ef4435f4c9666879ea460f49634cd856f819d65
Zuriel <oscarzu@me.com>
2019-09-12 20:38:31 -0500

    Coding the first steps of the task

--- PyBank.py
+++ PyBank.py
@@ -15,3 +15,13 @@ with open(csvpath, newline = '') as csvfile:
         Max_Revenue_Change = 0
         Min_Revenue_Change = 0
         Total_Revenue = 0
+
+        for row in csvreader:
+            
+            
+            Months = Months + 1
+            Total_Revenue = float(Total_Revenue) + float(row[1])
+            Present_Revenue = row[1]
+            Revenue_Change = float(Present_Revenue) - float(Previous_Revenue)
+            Total_Rev_Change = Total_Rev_Change + Revenue_Change
+            Previous_Revenue = row[1]
