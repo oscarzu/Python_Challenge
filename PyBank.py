a1337b3b08c279226a9ad83045eb8e44b01f52a9
Zuriel <oscarzu@me.com>
2019-09-12 20:35:34 -0500

    Setting the first variables

--- PyBank.py
+++ PyBank.py
@@ -7,3 +7,11 @@ with open(csvpath, newline = '') as csvfile:
         csvreader = csv.reader(csvfile, delimiter = ',')
         next(csvreader)
 
+        Months = 0
+        Total_Rev_Change = 0
+        Previous_Revenue = 0
+        Present_Revenue = 0
+        Revenue_Change = 0
+        Max_Revenue_Change = 0
+        Min_Revenue_Change = 0
+        Total_Revenue = 0
