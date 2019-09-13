492ee63d860792b0ebec5ddb54e9ba467f93011e
Zuriel <oscarzu@me.com>
2019-09-12 20:35:00 -0500

    First Lines of the code

--- PyBank.py
+++ PyBank.py
@@ -1,1 +1,9 @@
-csv_path=os.path.join("")
+import os
+import csv
+        
+csvpath = os.path.join("Resources","PyBank_Data.csv")
+
+with open(csvpath, newline = '') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter = ',')
+        next(csvreader)
+
